45cfa86906ace8f9721a7109f44ab2652238000f
RiddleBrooke68 <102398546+RiddleBrooke68@users.noreply.github.com>
2026-04-26 13:38:43 +1000

    tile war build_0_a1_02_ver12 pre04

---
+++ scripts/data/map_data.gd
@@ -0,0 +1,7 @@
+extends Resource
+class_name map_data
+
+@export var map_name : String
+@export var map_patern : TileMapPattern
+@export var map_scale : Vector2 = Vector2(1.0,1.0)
+@export var map_centre : Vector2i = Vector2i(-10,-13)
